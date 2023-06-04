bcecf13101919efe32b7f203f3f433d58f1924c8
KimHyeYoung <62800431+EngediKimHyeYoung@users.noreply.github.com>
2023-06-04 13:26:36 +0900

    2

---
+++ js/accordian_jquery2.js
@@ -0,0 +1,7 @@
+// // 제이쿼리 형태
+// $("#challengeSet>dt").on("click", function(){
+//     $("#challengeSet>dd").slideUp();
+//     $(this).next().stop().slideDown();
+//     $("#challengeSet>dt").removeClass("active");
+//     $(this).addClass("active");
+// });
